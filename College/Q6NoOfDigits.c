1d86c8f44d4edd7ca8b8da7e6f8f9d4a4deb136b
Tush N <143404072+TushN101@users.noreply.github.com>
2023-10-21 20:27:50 +0530

    Add files via upload

---
+++ College/Q6NoOfDigits.c
@@ -0,0 +1,19 @@
+//Program to count the number of digits
+#include <stdio.h>
+
+int main() {
+    int num;
+    int count = 0;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        num /= 10;
+        count++;
+    }
+
+    printf("Number of digits: %d\n", count);
+
+    return 0;
+}
